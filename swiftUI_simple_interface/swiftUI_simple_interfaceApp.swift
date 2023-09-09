c579e63be881f13760fdf30bb9fe241e60515a8e
Tema <asta.ru@yandex.ru>
2023-09-09 18:39:37 +0300

    Initial Commit

---
+++ swiftUI_simple_interface/swiftUI_simple_interfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_simple_interfaceApp.swift
+//  swiftUI_simple_interface
+//
+//  Created by Artem Ryabichev on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_simple_interfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
