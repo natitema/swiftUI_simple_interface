669ec702cc24e8cda6e8f4401a283e47e1cb4179
Tema <asta.ru@yandex.ru>
2023-09-15 15:40:12 +0300

    Authorization card interface.

--- swiftUI_simple_interface/ContentView.swift
+++ swiftUI_simple_interface/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("background")
+            Spacer()
+            HStack {
+                Image("avatar")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                VStack(alignment: .leading) {
+                    Text(email)
+                        .foregroundColor(.white)
+                        .padding(.bottom, 1)
+                    Text(password)
+                        .foregroundColor(.white)
+                }
+                .frame(maxHeight: 90, alignment: .top)
+                Spacer()
+            }
+            .padding(.bottom, 420)
+            .padding(.leading, 44)
+            
+            VStack {
+                VStack {
+                    TextField("Email", text: $email)
+                        .padding()
+                        .background(Color("textField"))
+                        .cornerRadius(10)
+                        .frame(height: 90)
+                    
+                    SecureField("Пароль", text: $password)
+                        .padding()
+                        .background(Color("textField"))
+                        .cornerRadius(10)
+                    
+                    Button(action: {}) {
+                        Text("Войти")
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                            .padding(.horizontal, 136)
+                            .background(Color("button"))
+                            .cornerRadius(10)
+                    }
+                    .frame(height: 90)
+                }
+                .padding(22)
+                .background(Color.white)
+                .cornerRadius(20)
+                .padding(.top, 130)
+                .padding(.horizontal, 14)
+                .padding(.bottom, 40)
+                
+                Text("Регистрация")
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
